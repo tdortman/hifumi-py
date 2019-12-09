ad58cfc8f90ad5de4e00742fbb35f4296e9d14bc
Tim Dortmann <git@tdortman.com>
2019-12-09 01:48:12 +0100

    shitty music bot, need to change lots of things but at least i made annie happy :3

--- ini2.py
+++ ini2.py
@@ -7,6 +7,10 @@ import qrcode
 import urbandict
 import urllib
 import operator
+from discord.utils import get
+import youtube_dl
+import os
+
 
 TOKEN = 'NjQxNDA5MzMwODg4ODM1MDgz.XcLHRQ.PvhkvwlbL0ZNU_cCccDxaiOnlCA'
 
@@ -41,6 +45,86 @@ reddit = praw.Reddit(client_id='ra7W9w_QZhwRaA',
 # '.QJXSOJ4jYV2ESYg8st7CW82OQTw')
 
 
+@bot.command(pass_context=True, aliases=['j', 'joi'])
+async def join(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    await voice.disconnect()
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"Joined {channel}")
+
+
+@bot.command(pass_context=True, aliases=['l', 'lea'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+        await ctx.send(f"Left {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("Don't think I am in a voice channel")
+
+
+@bot.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+            print("Removed old song file")
+    except PermissionError:
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
+        return
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([url])
+
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
+            os.rename(file, "song.mp3")
+
+    discord.utils.get(bot.voice_clients, guild=ctx.guild).play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: print("Song done!"))
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = 0.3
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
+
+
 @bot.command()
 async def urban(ctx, message):
     try:
@@ -63,12 +147,13 @@ async def calc(ctx, num1, operation, num2):
         '-': operator.sub,
         '*': operator.mul,
         '%': operator.mod,
-        '/': operator.floordiv
-
+        '/': operator.truediv,
+        '//': operator.floordiv,
+        '**': operator.pow
     }
-    #if operation in operators.keys():
-        #await ctx.channel.send(operators[operation](num1, num2)
 
+    if operation in operators:
+        print(operators[operation](int(num1), int(num2)))
 
 
 @bot.command()
