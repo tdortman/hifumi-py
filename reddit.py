53999e452434500e3fbadad430abad8b91ffc98f
Tim Dortmann <git@tdortman.com>
2021-03-02 16:10:04 +0100

    revamped code to work with asyncpraw

--- reddit.py
+++ reddit.py
@@ -1,10 +1,12 @@
-import praw
+import discord
+import asyncpraw
+import asyncprawcore
 import datetime
 import random
-import prawcore
-import main
+
 from tools import *
 from typing import *
+import main
 
 file = open(r"files/credentials.txt", "r")
 lines = file.readlines()
@@ -12,20 +14,28 @@ REDDIT_CLIENT_ID = lines[5].split()[1]
 REDDIT_CLIENT_SECRET = lines[6].split()[1]
 file.close()
 
-reddit = praw.Reddit(client_id=REDDIT_CLIENT_ID,
-                     client_secret=REDDIT_CLIENT_SECRET,
-                     user_agent='Mozilla/5.0')
+reddit = asyncpraw.Reddit(client_id=REDDIT_CLIENT_ID,
+                          client_secret=REDDIT_CLIENT_SECRET,
+                          user_agent='Mozilla/5.0')
 
 # reddit caches & checks for fetch function
 sub_cache_img = {}
 sub_cache_text = {}
 sub_running = False
 self_posts_running = False
+last_accessed = current_time()
+
+bot = None
+
+
+def get_client_var(client):
+    global bot
+    bot = client
 
 
 async def profile(message):
     try:
-        user = reddit.redditor(message.content.split()[1])
+        user = await reddit.redditor(message.content.split()[1], fetch=True)
         user_url = f"https://www.reddit.com/user/{user.name}"
         created_on = datetime.datetime.utcfromtimestamp(user.created_utc).strftime('%d/%m/%Y')
 
@@ -37,13 +47,13 @@ async def profile(message):
         embed = discord.Embed(
             title=user.name,
             description=desc,
-            color=0xce3a9b)
+            color=main.EMBED_COLOUR)
         embed.set_thumbnail(url=user.icon_img)
         await message.channel.send(embed=embed)
 
     except AttributeError:
         await message.channel.send("Seems like this user is banned!")
-    except prawcore.exceptions.NotFound:
+    except asyncprawcore.exceptions.NotFound:
         await message.channel.send("Invalid user! Make sure you typed the name correctly!")
     except Exception as e:
         await error_log(message, e)
@@ -58,7 +68,7 @@ async def sub(message, subreddit: str = None):
     :param subreddit: Subreddit to be accessed that allows for certain Subreddits to get their own "Command"
     :return: None
     """
-    global sub_running, sub_cache_img
+    global sub_running, sub_cache_img, last_accessed
     try:
         sub_running = True
         if subreddit:
@@ -66,17 +76,26 @@ async def sub(message, subreddit: str = None):
         else:
             sub_name = message.content.split()[1].lower()
 
+        tmp_accessed = current_time()
+
+        if last_accessed is not None:
+            if tmp_accessed - last_accessed >= 3600:
+                sub_cache_img = {}
+                last_accessed = tmp_accessed
+
         if '+' in sub_name or len(message.content.split()) == 3:
             if '+' in sub_name:
                 sub_name1 = sub_name.split('+')[0].lower()
                 sub_name2 = sub_name.split('+')[1].lower()
 
-            elif len(message.content.split()) == 3:
-                sub_name1 = message.content.split()[1]
-                sub_name2 = message.content.split()[2]
+                sub1 = await reddit.subreddit(sub_name.split('+')[0].lower(), fetch=True)
+                sub2 = await reddit.subreddit(sub_name.split('+')[1].lower(), fetch=True)
 
-            if (reddit.subreddit(sub_name1).over18 or reddit.subreddit(sub_name2).over18) and \
-                    not message.channel.is_nsfw():
+            elif len(message.content.split()) == 3:
+                sub1 = await reddit.subreddit(message.content.split()[1], fetch=True)
+                sub2 = await reddit.subreddit(message.content.split()[2], fetch=True)
+
+            if (sub1.over18 or sub2.over18) and not message.channel.is_nsfw():
                 await message.channel.send("One of the subreddits you're trying to access contains NSFW material, "
                                            "please move into a NSFW channel!")
                 return
@@ -85,8 +104,9 @@ async def sub(message, subreddit: str = None):
                     sub_cache_img = {}
 
                 if sub_name1 not in sub_cache_img:
-                    fetch_submissions(sub_name1, limit=50)
-                    fetch_submissions(sub_name2, limit=50, name=sub_name1)
+                    await fetch_submissions(sub_name1, limit=50)
+                    await fetch_submissions(sub_name2, limit=50, name=sub_name1)
+                    await reddit.close()
                     await message.channel.send(f'Results found: {len(sub_cache_img[sub_name1])}')
 
                 picture, name = random.choice(sub_cache_img[sub_name1])
@@ -102,7 +122,9 @@ async def sub(message, subreddit: str = None):
                 embed.set_image(url=picture)
                 await message.channel.send(embed=embed)
         else:
-            if reddit.subreddit(sub_name).over18 and not message.channel.is_nsfw():
+            sub_img = await reddit.subreddit(sub_name, fetch=True)
+
+            if sub_img.over18 and not message.channel.is_nsfw():
                 await message.channel.send("You can't access NSFW subreddits outside of NSFW channels!")
                 return
             else:
@@ -110,7 +132,7 @@ async def sub(message, subreddit: str = None):
                     sub_cache_img = {}
 
                 if sub_name not in sub_cache_img:
-                    fetch_submissions(sub_name, 50)
+                    await fetch_submissions(sub_name, 50)
                     await message.channel.send(f'Results found: {len(sub_cache_img[sub_name])}')
 
                 picture, name = random.choice(sub_cache_img[sub_name])
@@ -128,9 +150,9 @@ async def sub(message, subreddit: str = None):
 
             sub_running = False
 
-    except prawcore.exceptions.Redirect:
+    except asyncprawcore.exceptions.Redirect:
         await message.channel.send("That's not a valid subreddit!")
-    except (prawcore.exceptions.Forbidden, prawcore.exceptions.NotFound):
+    except (asyncprawcore.exceptions.Forbidden, asyncprawcore.exceptions.NotFound):
         await message.channel.send("Seems like this subreddit is either set to private or has been suspended.")
     except (KeyError, IndexError):
         await message.channel.send("No images found")
@@ -139,7 +161,7 @@ async def sub(message, subreddit: str = None):
 
 
 async def self_posts(message, subreddit: str = None):
-    global self_posts_running, sub_cache_text
+    global self_posts_running, sub_cache_text, last_accessed
     self_posts_running = True
     try:
         if subreddit:
@@ -147,20 +169,30 @@ async def self_posts(message, subreddit: str = None):
         else:
             sub_name = message.content.split()[2].lower()
 
-        if reddit.subreddit(sub_name).over18 and not message.channel.is_nsfw():
+        tmp_accessed = current_time()
+
+        if last_accessed is not None:
+            if tmp_accessed - last_accessed >= 3600:
+                sub_cache_text = {}
+                last_accessed = tmp_accessed
+
+        sub_txt = await reddit.subreddit(sub_name, fetch=True)
+
+        if sub_txt.over18 and not message.channel.is_nsfw():
             await message.channel.send("You can't access NSFW subreddits outside of NSFW channels!")
             return
         else:
             if len(sub_cache_text) == 10:
                 sub_cache_text = {}
+
             if sub_name not in sub_cache_text:
-                fetch_submissions(sub_name)
+                await fetch_submissions(sub_name)
                 await message.channel.send(f"Results found: {len(sub_cache_text[sub_name])}")
 
             url, title, selftext, upvotes, ratio, author = random.choice(sub_cache_text[sub_name])
 
             embed = discord.Embed(title=title, description=f"[Link to post by u/{author}]({url})\n\n{selftext}",
-                                  color=0xce3a9b)
+                                  color=main.EMBED_COLOUR)
             embed.set_footer(text=f"Upvotes: {upvotes}  Ratio: {adv_round(ratio * 100)}%")
             await message.channel.send(embed=embed)
 
@@ -168,15 +200,15 @@ async def self_posts(message, subreddit: str = None):
 
     except AttributeError:
         await self_posts(message)
-    except prawcore.exceptions.Redirect:
+    except asyncprawcore.exceptions.Redirect:
         await message.channel.send("That's not a valid subreddit!")
-    except prawcore.exceptions.Forbidden:
+    except asyncprawcore.exceptions.Forbidden:
         await message.channel.send("Seems like this subreddit is set to private.")
     except Exception as e:
         await error_log(message, e)
 
 
-def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = None):
+async def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = None):
     """
     Fetches Reddit posts from a specific subreddit into a dictionary
     So they can later be used for other commands
@@ -189,14 +221,16 @@ def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = No
     global sub_running, self_posts_running
 
     sub_name = subreddit.lower()
+    subreddit_obj = await reddit.subreddit(sub_name, fetch=True)
 
     if sub_running:
         sub_cache_img[sub_name] = []
 
-        submissions = list(reddit.subreddit(sub_name).new(limit=limit))
+        submissions = await reddit.subreddit(sub_name, fetch=True)
+        submissions_list = submissions.new(limit=limit)
 
-        for submission in submissions:
-            if submission.over_18 and not reddit.subreddit(sub_name).over18:
+        async for submission in submissions_list:
+            if submission.over_18 and not subreddit_obj.over18:
                 continue
             elif submission.url.split("/")[2] in ["i.redd.it", "i.imgur.com"]:
                 if name:
@@ -204,10 +238,11 @@ def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = No
                 else:
                     sub_cache_img[sub_name].append((submission.url, submission.title))
 
-        submissions = list(reddit.subreddit(sub_name).hot(limit=limit))
+        submissions = await reddit.subreddit(sub_name, fetch=True)
+        submissions_list = submissions.hot(limit=limit)
 
-        for submission in submissions:
-            if submission.over_18 and not reddit.subreddit(sub_name).over18:
+        async for submission in submissions_list:
+            if submission.over_18 and not subreddit_obj.over18:
                 continue
             elif submission.url.split("/")[2] in ["i.redd.it", "i.imgur.com"]:
                 if name:
@@ -215,10 +250,11 @@ def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = No
                 else:
                     sub_cache_img[sub_name].append((submission.url, submission.title))
 
-        submissions = list(reddit.subreddit(sub_name).top('all', limit=limit))
+        submissions = await reddit.subreddit(sub_name, fetch=True)
+        submissions_list = submissions.top('all', limit=limit)
 
-        for submission in submissions:
-            if submission.over_18 and not reddit.subreddit(sub_name).over18:
+        async for submission in submissions_list:
+            if submission.over_18 and not subreddit_obj.over18:
                 continue
             elif submission.url.split("/")[2] in ["i.redd.it", "i.imgur.com"]:
                 if name:
@@ -229,10 +265,11 @@ def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = No
     elif self_posts_running:
         sub_cache_text[sub_name] = []
 
-        submissions = list(reddit.subreddit(sub_name).new(limit=limit))
+        submissions = await reddit.subreddit(sub_name, fetch=True)
+        submissions_list = submissions.new(limit=limit)
 
-        for submission in submissions:
-            if submission.over_18 and not reddit.subreddit(sub_name).over18:
+        async for submission in submissions_list:
+            if submission.over_18 and not subreddit_obj.over18:
                 continue
             elif submission.is_self and len(submission.selftext) <= 2048:
                 if name:
@@ -242,10 +279,11 @@ def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = No
                     sub_cache_text[sub_name].append((submission.url, submission.title, submission.selftext,
                                                      submission.score, submission.upvote_ratio, submission.author.name))
 
-        submissions = list(reddit.subreddit(sub_name).hot(limit=limit))
+        submissions = await reddit.subreddit(sub_name, fetch=True)
+        submissions_list = submissions.hot(limit=limit)
 
-        for submission in submissions:
-            if submission.over_18 and not reddit.subreddit(sub_name).over18:
+        async for submission in submissions_list:
+            if submission.over_18 and not subreddit_obj.over18:
                 continue
             elif submission.is_self and len(submission.selftext) <= 2048:
                 if name:
@@ -255,10 +293,11 @@ def fetch_submissions(subreddit: str, limit: int = 100, name: Optional[str] = No
                     sub_cache_text[sub_name].append((submission.url, submission.title, submission.selftext,
                                                      submission.score, submission.upvote_ratio, submission.author.name))
 
-        submissions = list(reddit.subreddit(sub_name).top("all", limit=limit))
+        submissions = await reddit.subreddit(sub_name, fetch=True)
+        submissions_list = submissions.top("all", limit=limit)
 
-        for submission in submissions:
-            if submission.over_18 and not reddit.subreddit(sub_name).over18:
+        async for submission in submissions_list:
+            if submission.over_18 and not subreddit_obj.over18:
                 continue
             elif submission.is_self and len(submission.selftext) <= 2048:
                 if name:
